158cc0ea1524e3e61a63d99e7e980bcf5a133bf3
Harel Sultan <harel714@gmail.com>
2023-05-17 14:48:38 +0300

    Removed non-existing imports

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const stayRoutes = require('./api/stay/stay.routes')
 const reservationRoutes = require('./api/reservation/reservation.routes')
-const { setupSocketAPI } = require('./services/socket.service')
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
@@ -34,7 +33,6 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/stay', stayRoutes)
 app.use('/api/reservation', reservationRoutes)
-setupSocketAPI(http)
 
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
