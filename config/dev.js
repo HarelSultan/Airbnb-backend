443c69e74f5d454b0b70bc48aed95fd63bd4ebd6
Harel Sultan <harel714@gmail.com>
2023-05-17 14:14:29 +0300

    Changed config, first build.

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://harel714:harel714quix@cluster0.c5gzkwt.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'stay_db',
 }
